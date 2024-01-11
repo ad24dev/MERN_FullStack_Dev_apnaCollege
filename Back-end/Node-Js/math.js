e19eb8d4f1ca0e466bf696876f8aaa05fbb59245
ahmad <swat45726@gmail.com>
2024-01-12 02:53:29 +0500

    add new script.js file and learn module.exports & require

---
+++ Back-end/Node-Js/math.js
@@ -0,0 +1,25 @@
+const sum = (a, b) => a + b;
+const ml = (a, b) => a * b;
+const g = 9.8;
+const PI = 3.14;
+
+// we can exports the above data by different methods
+
+// *1
+//  const obj = {
+//   sum: sum,
+//   ML: ml,
+//   g: g,
+//   PI: PI,
+
+//  }
+// module.exports = obj;
+
+// *2
+// module.exports = "hello";
+
+// *3
+// exports.sum = (a, b) => a + b;
+// exports.ml = (a, b) => a * b;
+// exports.g = 9.8;
+// exports.PI = 3.14;
--- Back-end/Node-Js/script.js
+++ Back-end/Node-Js/script.js
@@ -1,5 +1,17 @@
-let n = 5;
+// let n = 5;
 
-for (let i = 0; i < n; i++) {
-  console.log("hello", i);
-}
+// for (let i = 0; i < n; i++) {
+//   console.log("hello", i);
+// }
+
+// let args = process.argv;
+
+// for (let i = 2; i < args.length; i++) {
+//   console.log("Hello to", args[i]);
+// }
+
+// ***** Exports in files ******
+
+const someValue = require("./math");
+
+console.log(someValue.sum(4, 2));
