1f055cdb4d9250ad0ab137f2b186fa5410902322
Ahmad Ali <aliahmad@Ahmads-MacBook-Air.local>
2024-10-06 15:06:01 +0500

    learning Response

--- Backend/express/index.js
+++ Backend/express/index.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const app = express();
 
-const port = 3000;
+const port = 8080;
 
 app.listen(port, () => {
   console.log(`app is listening on port ${port}`);
+});
+
+app.use((req, res) => {
+  // console.log("request recevied");
+  let code = "<h1>Furits</h1> <ul><li>apple</li><li>orange</li></ul>";
+  res.send(code);
 });
