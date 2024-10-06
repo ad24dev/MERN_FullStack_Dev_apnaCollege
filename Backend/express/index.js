a0d1fd8b6f0678c7ee6c288129bea88a929bafa8
Ahmad Ali <official.ad24dev@gmail.com>
2024-10-06 16:43:14 +0500

    learning routes

--- Backend/express/index.js
+++ Backend/express/index.js
@@ -11,17 +11,31 @@ app.get("/", (req, res) => {
   res.send("i am root");
 });
 
-app.get("/apple", (req, res) => {
-  res.send("you connected apple path");
+app.get("/:username/:id", (req, res) => {
+  let { username, id } = req.params;
+  res.send(`welcome to @${username}`);
 });
 
-app.get("/orange", (req, res) => {
-  res.send("you connected orange path");
+app.get("/search", (req, res) => {
+  console.log(req.query);
+  let { q } = req.query;
+  if (!q) {
+    res.send("please enter a search query");
+  }
+  res.send(`<h1>search results for ${q}</h1>`);
 });
 
-app.get("*", (req, res) => {
-  res.send("this path does not exist");
-});
+// app.get("/apple", (req, res) => {
+//   res.send("you connected apple path");
+// });
+
+// app.get("/orange", (req, res) => {
+//   res.send("you connected orange path");
+// });
+
+// app.get("*", (req, res) => {
+//   res.send("this path does not exist");
+// });
 
 // app.use((req, res) => {
 //   // console.log("request recevied");
