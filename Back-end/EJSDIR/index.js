046d51b63e1ddea59314592fe0b8765e87ddd5b4
Ahmad Ali <99157880+ad24dev@users.noreply.github.com>
2024-10-06 16:45:30 +0500

    Delete Back-end directory

--- Back-end/EJSDIR/index.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const app = express();
-
-let port = 8080;
-
-app.listen(port, () => {
-  console.log(`Listening on port ${port} `);
-});
--- Back-end/Node-Js/Furits/banana.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  name: "Banana",
-  color: "yellow",
-};
