cc78d2361ab57df93e2a7f9f4090904bb73724b3
Ahmad Ali <official.ad24dev@gmail.com>
2024-10-06 15:26:00 +0500

    installing nodemon

--- Backend/express/index.js
+++ Backend/express/index.js
@@ -7,8 +7,24 @@ app.listen(port, () => {
   console.log(`app is listening on port ${port}`);
 });
 
-app.use((req, res) => {
-  // console.log("request recevied");
-  let code = "<h1>Furits</h1> <ul><li>apple</li><li>orange</li></ul>";
-  res.send(code);
+app.get("/", (req, res) => {
+  res.send("i am root");
 });
+
+app.get("/apple", (req, res) => {
+  res.send("you connected apple path");
+});
+
+app.get("/orange", (req, res) => {
+  res.send("you connected orange path");
+});
+
+app.get("*", (req, res) => {
+  res.send("this path does not exist");
+});
+
+// app.use((req, res) => {
+//   // console.log("request recevied");
+//   let code = "<h1>Furits</h1> <ul><li>apple</li><li>orange</li></ul>";
+//   res.send(code);
+// });
